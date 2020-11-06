ca9719892861bf23122adfef6e3b3e3cabf7a38a
Nana Lau <nanalau315@hotmail.com>
2020-11-06 21:02:00 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,6 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  
   array.count do |word|
     word.class == String
   end
@@ -8,4 +9,6 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  
+  
 end
